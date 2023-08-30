609e2aa9a3b24f9708dfa6920b2b9ae38d88cfbb
Minims <github@minims.fr>
2023-08-30 22:22:58 +0200

    MyFox2MQTT 2023.9.1

--- myFox2Mqtt/main.py
+++ myFox2Mqtt/main.py
@@ -15,7 +15,7 @@ from myfox.sso import init_sso
 from myfox.api import MyFoxApi
 from myfox.websocket import MyFoxWebsocket
 
-VERSION = "2023.9.0"
+VERSION = "2023.9.1"
 
 
 def myfox_loop(config, mqtt_client, api):
